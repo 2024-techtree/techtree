3e1305300b5af97e9713126c3494542cbe5098d0
BuKwon Kim <dnu05043@naver.com>
2024-02-27 17:07:18 +0900

    start

---
+++ src/main/java/com/example/techtree/TechtreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.techtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechtreeApplication.class, args);
+    }
+
+}
