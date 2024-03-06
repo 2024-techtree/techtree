280ca7ab34eeb5fcd139d5e374c7974458729ada
전화철 <lunaris0206@naver.com>
2024-03-06 17:59:21 +0900

    marge develop

--- src/main/java/com/example/techtree/domain/inquiry/review/entity/Review.java
+++ src/main/java/com/example/techtree/domain/inquiry/review/entity/Review.java
@@ -1,29 +1,35 @@
 package com.example.techtree.domain.inquiry.review.entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column
-    private String title;
+	@Column
+	private String title;
 
-    @Column(columnDefinition = "TEXT")
-    private String content;
+	@Column(columnDefinition = "TEXT")
+	private String content;
 
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+	private LocalDateTime createDate;
+	private LocalDateTime modifyDate;
 }
