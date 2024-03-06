4873b55d411e86568bdc729db51291c61be04b0c
전화철 <lunaris0206@naver.com>
2024-03-06 16:29:06 +0900

    chore : 자잘한 디자인 수정, 경로 수정

--- src/main/java/com/example/techtree/domain/saving/goal/controller/GoalController.java
+++ src/main/java/com/example/techtree/domain/saving/goal/controller/GoalController.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/example/techtree/domain/saving/goal/dto/GoalDto.java
+++ src/main/java/com/example/techtree/domain/saving/goal/dto/GoalDto.java
@@ -1,14 +1,15 @@
 package com.example.techtree.domain.saving.goal.dto;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
