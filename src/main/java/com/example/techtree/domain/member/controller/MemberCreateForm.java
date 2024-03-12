845797b5008097cf518b27d314c6d4c4727b1272
ujgon <125572887+ujgon@users.noreply.github.com>
2024-03-12 14:39:27 +0900

    Janggon (#14)
    * feat:회원가입 기능
    
    * fix:회원가입 기능 수정
    
    * bug
    
    * fix:회원가입 기능 구현
    
    * fix:회원가입기능구현

---
+++ src/main/java/com/example/techtree/domain/member/controller/MemberCreateForm.java
@@ -0,0 +1,44 @@
+package com.example.techtree.domain.member.controller;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class MemberCreateForm {
+
+	@Size(min = 3, max = 25, message = "로그인ID는 3자 이상 25자 이하여야 합니다.")
+	@NotEmpty(message = "로그인ID는 필수항목입니다.")
+	private String login_id;
+
+	@NotEmpty(message = "비밀번호는 필수항목입니다.")
+	private String password1;
+
+	@NotEmpty(message = "비밀번호 확인은 필수항목입니다.")
+	private String password2;
+
+	@NotEmpty(message = "이메일은 필수항목입니다.")
+	@Email(message = "올바른 이메일 주소를 입력해주세요.")
+	private String email;
+
+	@NotEmpty(message = "이름은 필수항목입니다.")
+	private String username;
+
+	private LocalDate birthday;
+
+	private String phoneNumber;
+
+	private String profile;
+
+	private String profileImage;
+}
