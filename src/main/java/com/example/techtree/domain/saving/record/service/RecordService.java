3f58198a22332ae111ed9ceb5072032daf78d011
jyh1108 <kobcom12@naver.com>
2024-03-13 17:34:28 +0900

    feat : 저축 기록 목록 구현 미완

--- src/main/java/com/example/techtree/domain/saving/record/service/RecordService.java
+++ src/main/java/com/example/techtree/domain/saving/record/service/RecordService.java
@@ -2,7 +2,11 @@ package com.example.techtree.domain.saving.record.service;
 
 import com.example.techtree.domain.saving.record.dto.RecordDto;
 import com.example.techtree.domain.saving.record.entity.Record;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface RecordService {
 	Record savingRecordCreate(RecordDto recordDto);
+
+    Page<Record> getAllRecords(Pageable pageable);
 }
