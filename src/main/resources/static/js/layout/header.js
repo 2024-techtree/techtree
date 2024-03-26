bdea88c6479972911e7e0f265a2083bac9edf1b6
전화철 <lunaris0206@naver.com>
2024-03-26 20:06:15 +0900

    feat : 드롭다운 하위메뉴를 제외한 곳을 클릭시 드롭다운 닫히도록 기능 추가

--- src/main/resources/static/js/layout/header.js
+++ src/main/resources/static/js/layout/header.js
@@ -1,6 +1,18 @@
 var openDropdownId = null;
 
 // 드롭다운
+
+document.addEventListener('click', function (event) {
+    // 클릭된 요소가 드롭다운 메뉴의 일부인지 확인
+    var isDropdownButton = event.target.matches('.nav-btn') || event.target.closest('.nav-dropdown') !== null;
+
+    if (!isDropdownButton && openDropdownId !== null) {
+        // 활성화된 드롭다운 메뉴가 있고, 클릭된 요소가 드롭다운 메뉴의 일부가 아니라면 드롭다운을 닫는다.
+        document.getElementById(openDropdownId).style.display = 'none';
+        openDropdownId = null;
+    }
+});
+
 function toggleDropdown(id) {
     var dropdown = document.getElementById(id);
 
